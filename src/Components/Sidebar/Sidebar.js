98a0ee1431739d42887ce825895405d3846ce5a5
unknown <tarunvravi@gmail.com>
2021-02-12 15:24:59 -0500

    finished layout of the 3 sections

---
+++ src/Components/Sidebar/Sidebar.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './Sidebar.css'
+function Sidebar() {
+    return (
+        <div className="Sidebar">
+            <h1>Hello</h1>
+            <h1>Hello</h1>
+            <h1>Hello</h1>
+            <h1>Hello</h1>
+        </div>
+    )
+}
+
+export default Sidebar
---
+++ src/Components/Topics/Topics.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Topics.css'
+
+function Topics() {
+    return (
+        <div className="Topics">
+            
+        </div>
+    )
+}
+
+export default Topics
