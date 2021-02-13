593aa5c0b402abb7d8ab24036abc4fe1db7f325e
unknown <tarunvravi@gmail.com>
2021-02-13 02:17:52 -0500

    sidebar components

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import Sidebar from './Components/Sidebar/Sidebar'
 import Topics from './Components/Topics/Topics'
 import Widgets from './Components/Widgets/Widgets'
+
 function App() {
   return (
     <div class="app">
--- src/Components/Sidebar/Sidebar.js
+++ src/Components/Sidebar/Sidebar.js
@@ -1,12 +1,31 @@
 import React from 'react'
 import './Sidebar.css'
+import SidebarOption from './SidebarOption/SidebarOption'
+
+import LanguageIcon from '@material-ui/icons/Language';
+import SaveIcon from '@material-ui/icons/Save';
+import FlagIcon from '@material-ui/icons/Flag';
+import PublicIcon from '@material-ui/icons/Public';
+import RoomIcon from '@material-ui/icons/Room';
+import NewReleasesIcon from '@material-ui/icons/NewReleases';
+import SettingsIcon from '@material-ui/icons/Settings';
 function Sidebar() {
     return (
         <div className="Sidebar">
-            <h1>Hello</h1>
-            <h1>Hello</h1>
-            <h1>Hello</h1>
-            <h1>Hello</h1>
+            <div className="topBar">
+                <h1>Anti Bias Bot</h1>
+            </div>
+            <div className="options">
+                <SidebarOption text="Top Topics" active Icon={LanguageIcon}/>
+                <SidebarOption text="Saved Topics" Icon={SaveIcon}/>
+                <div className="divider" />
+                <SidebarOption text="U.S." Icon={FlagIcon}/>
+                <SidebarOption text="World" Icon={PublicIcon}/>
+                <SidebarOption text="Local" Icon={RoomIcon}/>
+                <SidebarOption text="Covid" Icon={NewReleasesIcon}/>
+                <div className="divider" />
+                <SidebarOption text="Settings" Icon={SettingsIcon}/>
+            </div>
         </div>
     )
 }
